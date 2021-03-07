ccbb49701978596fd2ad92e70843f93882580acc
Steve Smith <28569967+semuadmin@users.noreply.github.com>
2021-03-07 14:48:36 +0000

    Update nmeastreamer.py example to include POLLS

--- examples/nmeastreamer.py
+++ examples/nmeastreamer.py
@@ -1,11 +1,24 @@
 """
-Example implementation of a threaded NMEAMessage streamer
+Example implementation of a threaded NMEAMessage streamer.
 
 Connects to the receiver's serial port and sets up a
-threaded NMEAReader process. It waits for 10 seconds,
-then terminates the thread.
+daemon NMEAReader thread. While the thread is running in the
+background, it sends a series of NMEA RMC POLL requests
+corresponding to different GNSS constellations ('GPQ' = GPS, 
+'GLQ' = GLONASS, etc.).
 
-Created on 2 Oct 2020
+If the POLL is accepted, you'll see an RMC message in response (mixed
+in with whatever other messages the receiver is sending periodically).
+
+If the POLL is rejected, you'll typically get a TXT response:
+"<NMEA(GNTXT, numMsg=1, msgNum=1, msgType=1, text=NMEA unknown msg)>"
+
+The example is purely illustrative and the responses will depend
+on your receiver's specific configuration and capabilities. A standard
+domestic GPS receiver may typically only respond to the generic 'GNQ'
+(any GNSS) POLL.
+
+Created on 7 Mar 2021
 
 @author: semuadmin
 """
@@ -15,10 +28,10 @@ from io import BufferedReader
 from threading import Thread
 from time import sleep
 
-from pynmeagps.nmeareader import NMEAReader
+from pynmeagps import NMEAReader, NMEAMessage, POLL
 from serial import Serial, SerialException, SerialTimeoutException
 
-import pynmeagps.exceptions as ube
+import pynmeagps.exceptions as nme
 
 
 class NMEAStreamer:
@@ -33,7 +46,7 @@ class NMEAStreamer:
 
         self._serial_object = None
         self._serial_thread = None
-        self._ubxreader = None
+        self._nmeareader = None
         self._connected = False
         self._reading = False
         self._port = port
@@ -57,7 +70,7 @@ class NMEAStreamer:
             self._serial_object = Serial(
                 self._port, self._baudrate, timeout=self._timeout
             )
-            self._ubxreader = NMEAReader(BufferedReader(self._serial_object), False)
+            self._nmeareader = NMEAReader(BufferedReader(self._serial_object), False)
             self._connected = True
         except (SerialException, SerialTimeoutException) as err:
             print(f"Error connecting to serial port {err}")
@@ -122,14 +135,14 @@ class NMEAStreamer:
         while self._reading and self._serial_object:
             if self._serial_object.in_waiting:
                 try:
-                    (raw_data, parsed_data) = self._ubxreader.read()
+                    (raw_data, parsed_data) = self._nmeareader.read()
                     if parsed_data:
                         print(parsed_data)
                 except (
-                    ube.NMEAStreamError,
-                    ube.NMEAMessageError,
-                    ube.NMEATypeError,
-                    ube.NMEAParseError,
+                    nme.NMEAStreamError,
+                    nme.NMEAMessageError,
+                    nme.NMEATypeError,
+                    nme.NMEAParseError,
                 ) as err:
                     print(f"Something went wrong {err}")
                     continue
@@ -146,17 +159,22 @@ if __name__ == "__main__":
     TIMEOUT = 1
 
     print("Instantiating NMEAStreamer class...")
-    ubp = NMEAStreamer(PORT, BAUDRATE, TIMEOUT)
+    nms = NMEAStreamer(PORT, BAUDRATE, TIMEOUT)
     print(f"Connecting to serial port {PORT} at {BAUDRATE} baud...")
-    ubp.connect()
+    nms.connect()
     print("Starting reader thread...")
-    ubp.start_read_thread()
+    nms.start_read_thread()
 
     # DO OTHER STUFF HERE WHILE THREAD RUNS IN BACKGROUND...
-    sleep(10)
+    for mid in ('GAQ', 'GBQ', 'GLQ', 'GNQ', 'GPQ', 'GQQ'):
+        print(f"\nSending a {mid} message to poll for an RMC response.")
+        print("Look out for an RMC (known) or TXT (unknown) message in the input stream...\n")
+        msg = NMEAMessage('EI', mid, POLL, msgId='RMC')
+        nms.send(msg.serialize())
+        sleep(3)
 
     print("\n\nStopping reader thread...")
-    ubp.stop_read_thread()
+    nms.stop_read_thread()
     print("Disconnecting from serial port...")
-    ubp.disconnect()
+    nms.disconnect()
     print("Test Complete")
